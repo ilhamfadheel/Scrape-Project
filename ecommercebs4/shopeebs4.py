19d2db2368f1df613545609e8999e83897adc3ce
Ilham <ilham.sykes@hotmail.com>
2020-11-20 17:37:34 +0700

    beautifulsoup script update

--- ecommercebs4/shopeebs4.py
+++ ecommercebs4/shopeebs4.py
@@ -8,7 +8,8 @@ from time import sleep
 
 # create object for chrome options
 chrome_options = Options()
-base_url = 'https://shopee.co.id/aldmond22/'
+shop_name= 'aldmond22'
+base_url = f'https://shopee.co.id/{shop_name}'
 
 # set chrome driver options to disable any popup's from the website
 # to find local path for chrome profile, open chrome browser
@@ -16,15 +17,75 @@ base_url = 'https://shopee.co.id/aldmond22/'
 chrome_options.add_argument('disable-notifications')
 chrome_options.add_argument('--disable-infobars')
 chrome_options.add_argument('start-maximized')
-chrome_options.add_argument('user-data-dir=C:\\Users\\username\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
+#mind the directory below 'user-data-dir=C:\\Users\\username\\AppData\\Local\\Google\\Chrome\\User Data\\Default'
+chrome_options.add_argument("user-data-dir=C:\\Users\\Nayla Syafa\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
 # To disable the message, "Chrome is being controlled by automated test software"
-chrome_options.add_argument("disable-infobars")
+chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
 # Pass the argument 1 to allow and 2 to block
 chrome_options.add_experimental_option("prefs", { 
     "profile.default_content_setting_values.notifications": 2
     })
-# invoke the webdriver
-browser = webdriver.Chrome(executable_path = '../chromedriver_linux64/chromedriver',
+# invoke the webdriver (MIND THE CHROMEDRIVER FILE according to your OS)
+browser = webdriver.Chrome(executable_path = r"..\chromedriver\chromedriver.exe",
                           options = chrome_options)
 browser.get(base_url)
-delay = 5 #secods
+delay = 5 #seconds
+
+#----------- create table below
+
+# declare empty lists
+item_cost, item_init_cost, item_loc = [],[],[]
+item_name, items_sold, discount_percent = [], [], []
+while True:
+    try:
+        WebDriverWait(browser, delay)
+        print ("Page is ready")
+        sleep(5)
+        html = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+        #print(html)
+        soup = bs(html, "html.parser")
+
+        # find_all() returns an array of elements. 
+        # We have to go through all of them and select that one you are need. And than call get_text()
+        for item_n in soup.find_all('div', class_='_1NoI8_ _16BAGk'):
+            print(item_n.get_text())
+            item_name.append(item_n.text)
+
+        # find the price of items
+        for item_c in soup.find_all('span', class_='_341bF0'):
+            print(item_c.get_text())
+            item_cost.append(item_c.text)
+
+        # find initial item cost
+        for item_ic in soup.find_all('div', class_ = '_1w9jLI QbH7Ig U90Nhh'):
+            print(item_ic.get_text())
+            item_init_cost.append(item_ic.text)
+        # find total number of items sold/month
+        for items_s in soup.find_all('div',class_ = '_18SLBt'):
+            print(items_s.get_text())
+            items_sold.append(items_s.text)
+
+        # find item discount percent
+        for dp in soup.find_all('span', class_ = 'percent'):
+            print(dp.get_text())
+            discount_percent.append(dp.text)
+        # find item location
+        for il in soup.find_all('div', class_ = '_3amru2'):
+            print(il.get_text())
+            item_loc.append(il.text)
+
+        break # it will break from the loop once the specific element will be present. 
+    except TimeoutException:
+        print ("Loading took too much time!-Try again")
+
+rows = zip(item_name, item_init_cost,discount_percent,item_cost,items_sold,item_loc)
+
+import csv
+newFilePath = f'shopee_{shop_name}_item_list.csv'
+with open(newFilePath, "wb") as myfile:
+    writer = csv.writer(myfile)
+    for row in rows:
+        writer.writerow(row)
+
+# close the automated browser
+browser.close()
--- ecommercescrapy/ecommercescrapy/spiders/shopee_spider.py
+++ ecommercescrapy/ecommercescrapy/spiders/shopee_spider.py
@@ -1,15 +1,30 @@
 import scrapy
-
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from time import sleep
 
 class ShopeeSpider(scrapy.Spider):
-    name = "shopee"
-
-    def start_requests(self):
-        urls = [
+    name = "shopee_spider"
+    allowed_domains = ['shopee.co.id']
+    start_urls = [
             'http://shopee.co.id/aldmond22/'
         ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    chrome_options = Options()
+    chrome_options.add_argument('disable-notifications')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument('start-maximized')
+    chrome_options.add_argument('user-data-dir=C:\\Users\\Nayla Syafa\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1')
+    chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    chrome_options.add_experimental_option("prefs", { 
+    "profile.default_content_setting_values.notifications": 2
+    })
+#    self.driver = webdriver.Chrome(executable_path = '../chromedriver/chromedriver.exe',
+#                          options = chrome_options)
+#    def __init__(self):
+        
 
     def parse(self, response):
         page = response.url.split("/")[-2]
